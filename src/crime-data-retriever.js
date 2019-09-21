4b0d6f880a3ae9880dbd3a8d799fe261232a5c3e
Cory Nanni <cory.nanni@gmail.com>
2019-09-21 15:22:01 -0400

    built skeleton of crime-data-retriever, api is not functional yet

--- src/crime-data-retriever.js
+++ src/crime-data-retriever.js
@@ -1,19 +1,41 @@
-const ENDPOINT = "https://api.usa.gov/crime/fbi/sapi/api/";
-const APIKEY = "t2dI6umiBl8mNlfa1Ut2VPHzKyujCotXYvUNTIVP"; // hash this.
+const ENDPOINT = "https://api.usa.gov/crime/fbi/sapi/api/explorer";
+const APIKEY = "t2dI6umiBl8mNlfa1Ut2VPHzKyujCotXYvUNTIVP";
 
 const crimeData = (location, dates) => {
-  // takes in a location object with a latitude and longitude. Takes in an array of dates.
+  // takes in a location object with a string, a state. Takes in an array of dates.
+  const datesData = dates.forEach(date => {
+    return new Promise(function(resolve, reject) {
+      const dateData = retrieveCrimeDataFromDate(location, date);
+      resolve(dateData);
+    });
+  });
 
-  //const crimePromises = Promise.all();
+  // Produces an array of results for each date.
+  Promise.all(datesData)
+    .then(data => {
+      return data;
+    })
+    .error(err => {
+      console.log("error in crime-data-retriever");
+    });
   let data = retrieveCrimeDataFromDate();
   return data;
 };
 
-function retrieveCrimeDataFromDate(location) {
-  // takes in a location object with a latitude and longitude.
+function retrieveCrimeDataFromDate(location, crimeType) {
+  // takes in a location object with a string, a state.
   // Calls Crime Data Explorer API for crime data. Returns this data.
 
-  //fetch(`${endpointUrl}/${location}`)
+  fetch(`${ENDPOINT}/${location}/${crimeType}?api_key=${APIKEY}`)
+    .then(res => {
+      res.json();
+    })
+    .then(res => {
+      res.data;
+    })
+    .error(err => {
+      console.log("error in crime-data-retriever");
+    });
   return "";
 }
 

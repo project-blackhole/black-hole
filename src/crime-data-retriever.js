b3efd825127aaa0d7798bf4e44779fb5e72a51df
Cory Nanni <cory.nanni@gmail.com>
2019-09-21 15:36:56 -0400

    new crime data api implemented

--- src/crime-data-retriever.js
+++ src/crime-data-retriever.js
@@ -1,6 +1,3 @@
-const ENDPOINT = "https://api.usa.gov/crime/fbi/sapi/api/explorer";
-const APIKEY = "t2dI6umiBl8mNlfa1Ut2VPHzKyujCotXYvUNTIVP";
-
 const crimeData = (location, dates) => {
   // takes in a location object with a string, a state. Takes in an array of dates.
   const datesData = dates.forEach(date => {
@@ -22,19 +19,25 @@ const crimeData = (location, dates) => {
   return data;
 };
 
-function retrieveCrimeDataFromDate(location, crimeType) {
+function retrieveCrimeDataFromDate(location, date) {
   // takes in a location object with a string, a state.
   // Calls Crime Data Explorer API for crime data. Returns this data.
 
-  fetch(`${ENDPOINT}/${location}/${crimeType}?api_key=${APIKEY}`)
-    .then(res => {
-      res.json();
+  fetch(
+    `https://jgentes-crime-data-v1.p.rapidapi.com/crime?startdate=${date}&enddate=${date}&lat=${location.lat}&long=${location.long}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "jgentes-Crime-Data-v1.p.rapidapi.com",
+        "x-rapidapi-key": "b8fe8cc8bbmsh003ae401208d014p11b8bejsn071a9257dcc9"
+      }
+    }
+  )
+    .then(response => {
+      console.log(response);
     })
-    .then(res => {
-      res.data;
-    })
-    .error(err => {
-      console.log("error in crime-data-retriever");
+    .catch(err => {
+      console.log(err);
     });
   return "";
 }

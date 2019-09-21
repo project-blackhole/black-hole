9d6e0ec5aacbecf175cac9b6b077a24ac144fc1b
Maude <msimpson@medthink.com>
2019-09-21 15:13:45 -0400

    map component

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import * as VueGoogleMaps from "vue2-google-maps";
+
 
 Vue.config.productionTip = false
 
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyB6jwPxV3LECf2LsU7Ee3vVtzrYuBNgdag",
+    libraries: "places" // necessary for places input
+  }
+});
 new Vue({
   router,
   vuetify,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,73 @@
 <template>
-  <HelloWorld />
-</template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
 
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Project Black Hole
+        </h1>
+        <p class="subheading font-weight-regular">
+            What is the correlation of crime and the lunar cycle 
+        </p>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
 <script>
-import HelloWorld from '../components/HelloWorld';
+import Map from '../components/Map';
+
 
 export default {
   components: {
-    HelloWorld,
+    Map,
   },
 };
 </script>
